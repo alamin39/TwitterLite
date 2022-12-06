a20077173764115ecef5140b10ce54c417d90154
Al-Amin <alamin61a@gmail.com>
2022-12-06 12:06:16 +0900

    Initial Commit

---
+++ TwitterLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterLite
+//
+//  Created by Al-Amin on 6/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
